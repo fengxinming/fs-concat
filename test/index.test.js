37ff4670df8a12308de7b5c8ae9a9b6d443a1b9e
Jesse Feng <fxm0016@126.com>
2021-02-28 22:44:17 +0800

    Create index.test.js

---
+++ test/index.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { join } = require('path');
+const { statSync, readdirSync, unlinkSync } = require('fs');
+const concat = require('../src/index');
+
+describe('测试 concat', () => {
+  it('对比文件大小', () => {
+    let size = 0;
+    const fileList = [];
+    readdirSync(join(__dirname, 'files'), 'utf8').filter(n => n !== 'index.js').forEach((file) => {
+      file = join(__dirname, 'files', file);
+      fileList.push(file);
+      size += statSync(file).size;
+    });
+    const dest = join(__dirname, 'files/index.js');
+    unlinkSync(dest);
+    return concat(fileList, dest).then(() => {
+      expect(statSync(dest).size).toBe(size);
+    });
+  });
+
+  it('测试异常情况', () => {
+    expect(concat()).rejects.toThrow();
+    expect(concat([])).rejects.toThrow();
+    expect(concat([123])).rejects.toThrow();
+  });
+});
