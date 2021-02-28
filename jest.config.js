dbf0354ea10144a70fcd1ed8905d51e0fdec4dc9
Jesse Feng <fxm0016@126.com>
2021-02-28 23:04:54 +0800

    Create jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  roots: ['test'],
+  verbose: false,
+  testEnvironment: 'node',
+  // testEnvironment: 'jsdom',
+  testRegex: 'test/(.*/)*.*test.js$',
+  coverageDirectory: './coverage/',
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*'],
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
